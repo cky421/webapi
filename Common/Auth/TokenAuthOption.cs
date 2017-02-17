907cf5205d09db842b57121b8c35881a83b71bf3
tianjyan <tianjyan@cisco.com>
2017-02-17 17:54:07 +0800

    First Commit

--- Common/Auth/TokenAuthOption.cs
+++ Common/Auth/TokenAuthOption.cs
@@ -1,16 +1,16 @@
 using System;
 using Microsoft.IdentityModel.Tokens;
+using webapi.Common;
 
 namespace WebApi.Common.Auth
 {
     public class TokenAuthOption 
     { 
-        public static string Audience { get; } = "home_user"; 
-        public static string Issuer { get; } = "youngytj@sina.com"; 
+        public static string Audience { get; } = Config.IdentityAudience; 
+        public static string Issuer { get; } = Config.IdentityIssuer; 
         public static RsaSecurityKey Key { get; } = new RsaSecurityKey(RSAKeyHelper.GenerateKey()); 
         public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature); 
- 
         public static TimeSpan ExpiresSpan { get; } = TimeSpan.FromMinutes(1); 
-        public static string TokenType { get; } = "Bearer";  
+        public static string TokenType { get; } = Config.IdentityType;  
     } 
 }
