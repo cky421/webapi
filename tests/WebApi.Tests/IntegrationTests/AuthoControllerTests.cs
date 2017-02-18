64fa1ca82a7074b6b50076c6c04a25475d8ac1a7
youngytj <youngytj@sina.com>
2017-02-18 10:27:57 +0800

    Add Unit Test

---
+++ tests/WebApi.Tests/IntegrationTests/AuthoControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingControllersSample.Tests.IntegrationTests
+{
+    public class AuthoControllerTests
+    {
+    }
+}
