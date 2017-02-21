6ae1bfce530366b6284a39f244ec36981fb4e9bd
tianjyan <tianjyan@cisco.com>
2017-02-21 14:59:31 +0800

    Fix build

--- tests/WebApi.Tests/UnitTests/AuthControllerTests.cs
+++ tests/WebApi.Tests/UnitTests/AuthControllerTests.cs
@@ -2,9 +2,9 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using Newtonsoft.Json;
 using WebApi.Common;
-using WebApi.Controllers;
-using WebApi.Models;
+using WebApi.Controllers.V1;
 using WebApi.Models.Mongodb;
+using WebApi.Models.Requests;
 using WebApi.Models.Responses;
 using WebApi.Repositories.Interfaces;
 using Xunit;
@@ -17,16 +17,16 @@ namespace WebApi.Tests.UnitTests
         public void TestGetAuthToken()
         {
             var mockRepo = new Mock<IUserRepository>();
-            var user = new User
-            {
-                Username = Config.AdminName,
-                Password = Config.AdminPwd
-            };
+            var user = new User();
             mockRepo.Setup(repo => repo.Find(Config.AdminName, Config.AdminPwd)).Returns(user);
             var mockLog = new Mock<ILogger<AuthController>>();
 
             var auth = new AuthController(mockRepo.Object, mockLog.Object);
-            var result = auth.Post(user);
+            var result = auth.Post(new AuthRequest
+            {
+                Username = Config.AdminName,
+                Password = Config.AdminPwd
+            });
 
             var response = JsonConvert.DeserializeObject<Response<AuthResponse>>(result);
             Assert.Null(response.Message);
