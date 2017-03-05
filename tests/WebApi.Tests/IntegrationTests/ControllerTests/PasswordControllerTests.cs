29bc2582c38a899b7834c7d66a14849d035eb67e
youngytj <youngytj@sina.com>
2017-03-05 17:34:31 +0800

    Add Password Test

---
+++ tests/WebApi.Tests/IntegrationTests/ControllerTests/PasswordControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using WebApi.Tests.IntegrationTests.TestFixtures;
+using Xunit;
+
+namespace WebApi.Tests.IntegrationTests.ControllerTests
+{
+    public class PasswordControllerTests : IClassFixture<TestPasswordFixture>
+    {
+        private readonly HttpClient _client;
+
+        public PasswordControllerTests(TestFixture<Startup> fixture)
+        {
+            _client = fixture.Client;
+        }
+    }
+}
---
+++ tests/WebApi.Tests/IntegrationTests/TestFixtures/TestPasswordFixture.cs
@@ -0,0 +1,35 @@
+﻿using WebApi.Common;
+using WebApi.Repositories;
+using WebApi.Repositories.Interfaces;
+
+namespace WebApi.Tests.IntegrationTests.TestFixtures
+{
+    public class TestPasswordFixture : TestFixture<Startup>
+    {
+        public string AdminId { get; set; }
+
+        private readonly IGroupRepository _groupRepository;
+        private readonly IPasswordRepository _passwordRepository;
+
+        public TestPasswordFixture() : base()
+        {
+            IUserRepository userRepository = new UserRepository();
+            var admin = userRepository.Find(Config.AdminName, Config.AdminPwd);
+            AdminId = admin.UserId;
+
+            _groupRepository = new GroupRepository();
+            _groupRepository.InsertGroup(AdminId, "TestGroup1");
+            _groupRepository.InsertGroup(AdminId, "TestGroup2");
+            _groupRepository.InsertGroup(AdminId, "TestGroup3");
+
+            _passwordRepository = new PasswordRepository();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _groupRepository.Clear(AdminId);
+            _passwordRepository.Clear(AdminId);
+        }
+    }
+}
